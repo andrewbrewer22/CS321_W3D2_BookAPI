558f326c6e3db0cf4390fbf260b1552baaa104c5
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-04-18 21:26:57 -0700

    service completion

---
+++ CS321_W3D2_BookAPI/Services/AuthorService.cs
@@ -0,0 +1,61 @@
+﻿using CS321_W3D2_BookAPI.Data;
+using CS321_W3D2_BookAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS321_W3D2_BookAPI.Services
+{
+    public class AuthorService : IAuthorService
+    {
+
+        private readonly BookContext _bookContext;
+        public AuthorService(BookContext bookContext)
+        {
+            _bookContext = bookContext;
+        }
+        public Author Add(Author author)
+        {
+            _bookContext.Authors.Add(author);
+            _bookContext.SaveChanges();
+            return author;
+
+        }
+
+        public Author Get(int id)
+        {
+            return _bookContext.Authors.Find(id);
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            return _bookContext.Authors.ToList();
+        }
+
+        public void Remove(Author author)
+        {
+            _bookContext.Remove(author);
+            _bookContext.SaveChanges();
+        }
+
+        public Author Update(Author author)
+        {
+            var curremtAuthor = Get(author.Id);
+
+            if(curremtAuthor == null)
+            {
+                return null;
+            }
+
+            _bookContext.Entry(curremtAuthor)
+                .CurrentValues
+                .SetValues(author);
+
+            _bookContext.Update(curremtAuthor);
+            _bookContext.SaveChanges();
+
+            return curremtAuthor;
+        }
+    }
+}
